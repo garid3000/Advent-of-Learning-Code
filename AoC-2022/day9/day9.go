470b1b6f0b3eb25a0f97696cf3c1eda016f3e6c8
garid3000 <garidzorigoo@gmail.com>
2022-12-09 15:53:14 +0900

    Solved 2022-day9-part2

--- AoC-2022/day9/day9.go
+++ AoC-2022/day9/day9.go
@@ -10,31 +10,33 @@ import (
 
 
 type Coord struct {
-	x, y int
+	x,	y int;
 }
 
 var (
-	head_cur_coor = Coord{200,160}
-	tail_cur_coor = Coord{200,160}
+	//chain[0] = Coord{200,160}
+	//tail_cur_coor = Coord{200,160}
+
+	chain = [10] Coord {} 
 
 	maxheadx = 200 // i know this is stupid  // i should have extract this value dynamically
 	minheadx = 200
 	maxheady = 160
 	minheady = 160
 
-	coordinates_that_tail_visited [300][300] bool
+	coordinates_that_tail_visited [300][300][10] bool
 )
 
 func move_head(dir byte){
 	switch(dir){
 		case 'U':
-			head_cur_coor.y += 1 
+			chain[0].y += 1 
 		case 'L':
-			head_cur_coor.x -= 1 
+			chain[0].x -= 1 
 		case 'R':
-			head_cur_coor.x += 1 
+			chain[0].x += 1 
 		case 'D':
-			head_cur_coor.y -= 1 
+			chain[0].y -= 1 
 		default:
 			fmt.Printf("Bad dir:=%c", dir)
 			panic(1)
@@ -60,40 +62,43 @@ func is_tail_touched_head(tcoor, hcoor Coord) bool{
 
 
 
-func follow_tail(){
+func follow_the_lead(lead_coor, follower_coor Coord) Coord{
 	//if is_tail_touched_head() {
 	//	return //wihtout moving
 	//}
-	ver_dis := head_cur_coor.y - tail_cur_coor.y
-	hor_dis := head_cur_coor.x - tail_cur_coor.x
+	ver_dis := lead_coor.y - follower_coor.y
+	hor_dis := lead_coor.x - follower_coor.x
+	newFollower_coor := Coord{follower_coor.x, follower_coor.y}
 
 	if (abs(ver_dis) < 2) && (abs(hor_dis) < 2) {
-		return
+		return newFollower_coor
 		//no movement required
 	} else if (abs(ver_dis) == 2) && (hor_dis == 0) {
 		//only move vertically
-		if ver_dis > 0 {tail_cur_coor.y += 1}
-		if ver_dis < 0 {tail_cur_coor.y -= 1}
+		if ver_dis > 0 {newFollower_coor.y += 1}
+		if ver_dis < 0 {newFollower_coor.y -= 1}
+		return newFollower_coor
 	} else if (abs(hor_dis) == 2) && (ver_dis == 0) {
 		//only move horizontally
-		if hor_dis > 0 {tail_cur_coor.x += 1}
-		if hor_dis < 0 {tail_cur_coor.x -= 1}
+		if hor_dis > 0 {newFollower_coor.x += 1}
+		if hor_dis < 0 {newFollower_coor.x -= 1}
+		return newFollower_coor
 	} else {
 		//now the diagonale
-		tmp_coor0 := Coord{tail_cur_coor.x-1, tail_cur_coor.y-1}
-		tmp_coor1 := Coord{tail_cur_coor.x-1, tail_cur_coor.y+1}
-		tmp_coor2 := Coord{tail_cur_coor.x+1, tail_cur_coor.y-1}
-		tmp_coor3 := Coord{tail_cur_coor.x+1, tail_cur_coor.y+1}
+		tmp_coor0 := Coord{follower_coor.x-1, follower_coor.y-1}
+		tmp_coor1 := Coord{follower_coor.x-1, follower_coor.y+1}
+		tmp_coor2 := Coord{follower_coor.x+1, follower_coor.y-1}
+		tmp_coor3 := Coord{follower_coor.x+1, follower_coor.y+1}
 
-		if is_tail_touched_head(head_cur_coor, tmp_coor0) {tail_cur_coor = tmp_coor0; return}
-		if is_tail_touched_head(head_cur_coor, tmp_coor1) {tail_cur_coor = tmp_coor1; return}
-		if is_tail_touched_head(head_cur_coor, tmp_coor2) {tail_cur_coor = tmp_coor2; return}
-		if is_tail_touched_head(head_cur_coor, tmp_coor3) {tail_cur_coor = tmp_coor3; return}
+		if is_tail_touched_head(lead_coor, tmp_coor0) {return tmp_coor0}
+		if is_tail_touched_head(lead_coor, tmp_coor1) {return tmp_coor1}
+		if is_tail_touched_head(lead_coor, tmp_coor2) {return tmp_coor2}
+		if is_tail_touched_head(lead_coor, tmp_coor3) {return tmp_coor3}
 
 		// if left here panic
 		fmt.Printf("Couldn't find better diagonale move for tail\n")
 		fmt.Printf("Positions: head %v\ttail %v",
-			head_cur_coor, tail_cur_coor,
+			lead_coor, follower_coor,
 		)
 		panic(1)
 	} 
@@ -101,24 +106,34 @@ func follow_tail(){
 }
 
 func log_maxmin_of_head(){
-	if head_cur_coor.x > maxheadx {maxheadx = head_cur_coor.x}
-	if head_cur_coor.x < minheadx {minheadx = head_cur_coor.x}
-	if head_cur_coor.y > maxheady {maxheady = head_cur_coor.y}
-	if head_cur_coor.y < minheady {minheady = head_cur_coor.y}
+	if chain[0].x > maxheadx {maxheadx = chain[0].x}
+	if chain[0].x < minheadx {minheadx = chain[0].x}
+	if chain[0].y > maxheady {maxheady = chain[0].y}
+	if chain[0].y < minheady {minheady = chain[0].y}
 }
 
 
 func move(dir byte, steps int){
 	for i:=0; i<steps; i++{
 		move_head(dir) // move the head
-		follow_tail()
-		coordinates_that_tail_visited[tail_cur_coor.y][tail_cur_coor.x] = true
+		for j:=1; j<10; j++{ //for the chain
+			chain[j] = follow_the_lead(chain[j-1], chain[j])
+			//tail_cur_coor = follow_the_lead(chain[0], tail_cur_coor)
+			coordinates_that_tail_visited[chain[j].y][chain[j].x][j] = true
+		}
 		log_maxmin_of_head()
 	}
 }
 
 
 func main(){
+	//initial coordinates of chain
+	for i:=0; i<10; i++ {
+		chain[i].x = 200
+		chain[i].y = 160
+	}
+
+	
 	fmt.Printf("AoC2022\n");
 	fname := "/home/garid/Documents/advent/AoC-2022/day9/input.txt"
 	file, err:= os.Open(fname)
@@ -138,27 +153,32 @@ func main(){
 			break
 		}
 		fmt.Sscanf(line, "%c %d\n", &dir, &steps)
-		fmt.Printf("%d\t%c-%d| %v %v |->", i, dir, steps, head_cur_coor, tail_cur_coor)
+		fmt.Printf("%d\t%c-%d| %v %v |->", i, dir, steps, chain[0], chain[9])
 		move(dir, steps)
-		fmt.Printf("\t|%v %v\n",  head_cur_coor, tail_cur_coor)
+		fmt.Printf("\t|%v %v\n",  chain[0], chain[9])
 	}
 	//finished instructions
 
 	fmt.Printf("x:%d to %d\n", minheadx, maxheadx)
 	fmt.Printf("y:%d to %d\n", minheady, maxheady)
 
-	fmt.Printf("current head %v\n", head_cur_coor)
-	fmt.Printf("current tail %v\n", tail_cur_coor)
+	fmt.Printf("current head %v\n", chain[0])
+	fmt.Printf("current tail %v\n", chain[9])
 	//count tail visited
-	tailvisit:=0
+	visit_of_1th:=0
+	visit_of_9th:=0
 	for i:=0; i<300; i++{
 		for j:=0; j<300; j++{
-			if coordinates_that_tail_visited[i][j] {
-				tailvisit++;
+			if coordinates_that_tail_visited[i][j][1] {
+				visit_of_1th++;
+			}
+			if coordinates_that_tail_visited[i][j][9] {
+				visit_of_9th++;
 			}
 		}
 	}
 
-	fmt.Printf("tail has visited %v positions\n", tailvisit) //this is part1
+	fmt.Printf("1-th has visited %v positions\n", visit_of_1th) //this is part1
+	fmt.Printf("9-th has visited %v positions\n", visit_of_9th) //this is part2
 
 }
