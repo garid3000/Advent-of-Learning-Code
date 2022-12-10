9a0f50dfb69282bf17ccdc49fd1108ec1c2781e6
garid3000 <garidzorigoo@gmail.com>
2022-12-10 14:46:49 +0900

    Solved 2022-day9-p2

--- AoC-2022/day10/day10.go
+++ AoC-2022/day10/day10.go
@@ -8,19 +8,56 @@ import (
 )
 
 var (
-	X = 1
+	X = 1      // X means apperantly x-axis, sprite pos
 	cycle = 0
 	sum = 0
+	tmprow = [40]byte{'y'}
+	display = [6][40]byte {{'x'}}
+	row_count = 0
 )
 
+func abs(v int) int{
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func init_arrays(){ //not needed, just for the debuggin purposes
+	for i:=0;i<6;i++{
+		for j:=0;j<40;j++{
+			display[i][j] = 'x'
+		}
+	}
+}
+
+func display_message(){
+	for i:=0;i<6;i++{
+		for j:=0;j<40;j++{
+			fmt.Printf("%c", display[i][j])
+		}
+		fmt.Printf("\n")
+	}
+}
+
 func inc_cycle(){
+	pos := cycle % 40
+	if abs(pos - X) <= 1 {
+		tmprow[pos] = '#'
+	} else {
+		tmprow[pos] = ' '
+	}
+
 	cycle++
 	fmt.Printf("*")
 	if (cycle % 40) == 20{
 		fmt.Printf("----Cycle: %d, X:=%d-----", cycle, X)
 		sum += cycle * X
+	} else if (cycle % 40) == 0 {
+		for j:=0; j<40; j++{
+			display[cycle/40 - 1][j] = tmprow[j]
+		}
 	}
-	
 }
 
 func noop(){
@@ -55,6 +92,9 @@ func readline(line string){
 func main(){
 	//initial coordinates of chain
 	fmt.Printf("AoC2022 day 10\n");
+
+	init_arrays()
+	display_message()
 	fname := "/home/garid/Documents/advent/AoC-2022/day10/input.txt"
 	//fname := "/home/garid/Documents/advent/AoC-2022/day10/test1"
 	file, err:= os.Open(fname)
@@ -76,4 +116,5 @@ func main(){
 	}
 	//finished instructions
 	fmt.Printf("finished: sum=%d\n", sum) // this is part1
+	display_message()
 }
