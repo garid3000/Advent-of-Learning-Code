cc3aa5b8e2c36af4ec0f7e88ebd58c2b4ea25a1b
garid3000 <garidzorigoo@gmail.com>
2022-12-19 16:56:41 +0900

    Solved 2022-day18-p2

--- AoC-2022/day18/day18.go
+++ AoC-2022/day18/day18.go
@@ -17,6 +17,18 @@ type droplet struct{
 var(
 	alldroplets = make([]droplet, 0, 3000)
 	surfaces = 0
+	correct_surfaces = 0
+
+	max_x=0
+	min_x=0
+	max_y=0
+	min_y=0
+	max_z=0
+	min_z=0
+
+	grid      = [40][40][40]int{}
+	explored = [40][40][40]int{}
+	count_explored = 0
 )
 
 func abs(x int)int{
@@ -37,7 +49,70 @@ func appendDroplet(newDroplet droplet){
 	}
 	surfaces += tmp
 	alldroplets = append(alldroplets, newDroplet)
+
+	if newDroplet.x > max_x {max_x = newDroplet.x}
+	if newDroplet.x < min_x {min_x = newDroplet.x}
+	if newDroplet.y > max_y {max_y = newDroplet.y}
+	if newDroplet.y < min_y {min_y = newDroplet.y}
+	if newDroplet.z > max_z {max_z = newDroplet.z}
+	if newDroplet.z < min_z {min_z = newDroplet.z}
+
+	grid[newDroplet.x+10][newDroplet.y+10][newDroplet.z+10] = 1
 }
+
+func explore(x,y,z int){
+	if grid[x][y][z] == 1{
+		return 
+	} else if explored[x][y][z] == 1{
+		return // already explored
+	} else {
+		explored[x][y][z] = 1
+		count_explored++;
+		// set current cursor as explored and explore neighbors
+		//explore 8 neighbors
+		if x > 0  {explore(x-1, y, z)}
+		if x < 39 {explore(x+1, y, z)}
+
+		if y > 0  {explore(x, y-1, z)}
+		if y < 39 {explore(x, y+1, z)}
+		
+		if z > 0  {explore(x, y, z-1)}
+		if z < 39 {explore(x, y, z+1)}
+	}
+
+}
+
+func isThis_xyz_adroplet(x, y, z int) bool {
+	for _, droplet := range alldroplets{
+		if droplet.x == x && droplet.y ==y && droplet.z == z{
+			return true
+		}
+	}
+	return false
+}
+
+func count_outer_surface(){
+	var gx, gy, gz int
+	for _, droplet := range alldroplets{
+		gx, gy, gz =  droplet.x+10, droplet.y+10, droplet.z+10
+		if grid[gx+1][gy][gz]==0 && explored[gx+1][gy][gz]==1 {correct_surfaces++}
+		if grid[gx-1][gy][gz]==0 && explored[gx-1][gy][gz]==1 {correct_surfaces++}
+
+		if grid[gx][gy+1][gz]==0 && explored[gx][gy+1][gz]==1 {correct_surfaces++}
+		if grid[gx][gy-1][gz]==0 && explored[gx][gy-1][gz]==1 {correct_surfaces++}
+
+		if grid[gx][gy][gz+1]==0 && explored[gx][gy][gz+1]==1 {correct_surfaces++}
+		if grid[gx][gy][gz-1]==0 && explored[gx][gy][gz-1]==1 {correct_surfaces++}
+	}
+
+	// for x:=0; x<40; x++ {
+	// 	for y:=0; y<40; y++ {
+	// 		for z:=0; z<40; z++ {
+	// 		}
+	// 	}
+	// }
+}
+
 
 func main() {
 	fmt.Printf("AoC2022 day 18\n")
@@ -67,5 +142,15 @@ func main() {
 		appendDroplet(newDroplet)
 	}
 	fmt.Printf("%v\n", alldroplets)
-	fmt.Printf("%v\n", surfaces)
+	fmt.Printf("Surface:%v\n", surfaces)
+	fmt.Println("min, max:")
+	fmt.Println(min_x,max_x)
+	fmt.Println(min_y,max_y)
+	fmt.Println(min_z,max_z)
+
+	explore(0, 0, 0)
+	
+	fmt.Printf("explored: %v of %v\n", count_explored, 40*40*40)
+	count_outer_surface()
+	fmt.Printf("outer surface %v\n", correct_surfaces) 
 }
